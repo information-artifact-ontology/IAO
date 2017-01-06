cb33740410f1d3db890165db19e49f22d751ebd2
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-01-06 13:39:02 -0500

    remove spurious study design extra definition. Change code.google.com issue links to github issue links (should probably make them PURLs). Change other code.google.com refs to github refs

---
+++ src/ontology/ontoFox/clean-ontofox-results.pl
@@ -0,0 +1,5 @@
+while (<>) {last if /[<][!]-- / ; print $_}
+while (<>) {last if /[<][!]-- /;}
+print "<!-- ";
+while (<>)
+{ next if /a planned process that realizes the concretization of a study design/ ; print $_}
