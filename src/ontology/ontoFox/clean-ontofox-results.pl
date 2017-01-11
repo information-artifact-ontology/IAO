f3ce083938d947a628153c5a267d2ceade51e7f9
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-01-10 22:21:33 -0500

    Policy for ontology components of IAO. The ontology IRI never has /dev/ in it, but each files should have a *versionIRI* that does. That way it's easy to set things up so that there can be a live version - http://purl.obolibrary.org/obo/iao/dev/iao.owl catalog-v001.xml SHOULD NOT BE CHECKED IN Protege may write over it/change it and the release build depends not having protege screw it up. If you find it modified, just 'git checkout catalog-v0001.xml' to reset it.

--- src/ontology/ontoFox/clean-ontofox-results.pl
+++ src/ontology/ontoFox/clean-ontofox-results.pl
@@ -1,5 +1,19 @@
-while (<>) {last if /[<][!]-- / ; print $_}
+# Copy until the first xml comment, which leads into the annotation property section
+while (<>) 
+  { last if /[<][!]-- / ;
+    # Add versionIRI
+    if (m'<owl:Ontology rdf:about="http://purl.obolibrary.org/obo/iao/import-OBO.owl"/>')
+      { s'OBO.owl"/>'OBO.owl">';
+	print $_;
+	print '    <owl:versionIRI rdf:resource="http://purl.obolibrary.org/obo/iao/dev/import-OBO.owl"/>'."\n";
+	print '  </owl:Ontology>'."\n";
+      }
+    else {
+      print $_}
+  }
+# Skip over that section
 while (<>) {last if /[<][!]-- /;}
 print "<!-- ";
+# Remove a stale definition from OBI
 while (<>)
 { next if /a planned process that realizes the concretization of a study design/ ; print $_}
