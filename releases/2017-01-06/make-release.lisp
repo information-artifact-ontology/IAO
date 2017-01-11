db86cd8ee84c1f3411b97db6220de09db6d9658c
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-01-10 22:45:26 -0500

    hopefully versioniri crap sorted out

---
+++ releases/2017-01-06/make-release.lisp
@@ -0,0 +1,6 @@
+Made with LSW https://github.com/alanruttenberg/lsw2/
+Commit: d086ce140eae06571dc35b20a1308feb367f5465
+
+(make-release  "iao" "~/repos/information-artifact-ontology/src/ontology/iao.owl"
+               :when 3692716953
+               :additional-products '("ontology-metadata.owl" "iao-main"))
