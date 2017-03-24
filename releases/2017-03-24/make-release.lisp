52026d0a5f0860727e3789e670bf20e2c488f89a
Alan Ruttenberg <alanruttenberg@gmail.com>
2017-03-24 12:14:25 -0400

    New release to address https://github.com/information-artifact-ontology/IAO/issues/195. See issue and release notes for more detail

---
+++ releases/2017-03-24/make-release.lisp
@@ -0,0 +1,6 @@
+Made with LSW https://github.com/alanruttenberg/lsw2/
+Commit: b9c95fca1c54ebac3b4f154bc04e6c9cb3511f55
+
+(make-release  "iao" "~/repos/information-artifact-ontology/src/ontology/iao.owl"
+               :when 3692716953
+               :additional-products '("ontology-metadata.owl" "iao-main"))
